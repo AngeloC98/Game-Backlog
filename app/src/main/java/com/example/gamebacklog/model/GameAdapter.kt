df4438826b258f619444c44bbf485bed66793ae0
AngeloC98 <acabrasnl@gmail.com>
2020-06-11 19:21:55 +0200

    Games now load in on main activity

---
+++ app/src/main/java/com/example/gamebacklog/model/GameAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.gamebacklog.model
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gamebacklog.R
+import kotlinx.android.synthetic.main.item_game.view.*
+
+class GameAdapter(private val games: List<Game>, private val context: Context) :
+    RecyclerView.Adapter<GameAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView){
+
+        fun bind(game: Game){
+            itemView.tvTitle.text = game.title
+            itemView.tvPlatform.text = game.platform
+            itemView.tvDate.text = game.releaseDate
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_game, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return games.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(games[position])
+    }
+
+}
--- app/src/main/java/com/example/gamebacklog/ui/MainActivity.kt
+++ app/src/main/java/com/example/gamebacklog/ui/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.gamebacklog.ui
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gamebacklog.R
-
+import com.example.gamebacklog.model.Game
+import com.example.gamebacklog.model.GameAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainActivityViewModel: MainActivityViewModel by viewModels()
+    private val games = arrayListOf<Game>()
+    private val gameAdapter = GameAdapter(games, this)
+    val ADD_GAME_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +32,38 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         initViews()
+        observeViewModel()
     }
 
     private fun initViews() {
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        rvGames.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        rvGames.adapter = gameAdapter
+        rvGames.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+
+        fab.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivityForResult(intent, ADD_GAME_REQUEST_CODE)
+        }
+    }
+
+    private fun observeViewModel() {
+        mainActivityViewModel.games.observe( this, Observer { games ->
+            this@MainActivity.games.clear()
+            this@MainActivity.games.addAll( games )
+            gameAdapter.notifyDataSetChanged()
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == ADD_GAME_REQUEST_CODE) {
+                data?.let { newGame ->
+                    val game = newGame.getParcelableExtra<Game>("ADD_GAME")
+                    Log.d("GAME", game.toString())
+                    mainActivityViewModel.insertGame(game!!)
+                }
+            }
         }
     }
 
