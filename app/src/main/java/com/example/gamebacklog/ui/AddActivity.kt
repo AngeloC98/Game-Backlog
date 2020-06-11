cf2b56cbf9534d308847808e42c3258a3f7dfc91
AngeloC98 <acabrasnl@gmail.com>
2020-06-11 22:19:46 +0200

    Added date formatting

--- app/src/main/java/com/example/gamebacklog/ui/AddActivity.kt
+++ app/src/main/java/com/example/gamebacklog/ui/AddActivity.kt
@@ -2,16 +2,22 @@ package com.example.gamebacklog.ui
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gamebacklog.R
 import com.example.gamebacklog.model.Game
 
 import kotlinx.android.synthetic.main.activity_add.*
 import kotlinx.android.synthetic.main.content_add.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class AddActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
@@ -21,12 +27,18 @@ class AddActivity : AppCompatActivity() {
 
     }
 
+
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun initViews() {
+        val formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
+
         fab.setOnClickListener {
+            val date = LocalDate.of(etYear.text.toString().toInt(), etMonth.text.toString().toInt(), etDay.text.toString().toInt())
+            val formattedDate = date.format(formatter)
             val game = Game(
                 title = etTitle.text.toString(),
                 platform = etPlatform.text.toString(),
-                releaseDate = "${etDay.text} ${etMonth.text} ${etYear.text}"
+                releaseDate = formattedDate.toString()
             )
 
             val intent = Intent()
